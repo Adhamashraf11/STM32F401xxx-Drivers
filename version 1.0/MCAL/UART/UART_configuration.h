63a449fdc2fc3b69990ba2c3168d99d7aa3c5efe
Adham Ashraf Mohamed <115602981+Adhamashraf11@users.noreply.github.com>
2023-12-02 05:05:24 +0200

    Add files via upload

---
+++ version 1.0/MCAL/UART/UART_configuration.h
@@ -0,0 +1,19 @@
+/*
+ *  Name        : UART_config.h
+ *  Created on  : nov 25, 2023 
+ *  Description : this file contains the configration of UART prepherail
+ *  Version     : v1.0
+ *  Author      : Adham Ashraf Mohamed
+ *  LAYER       : MCAL
+ *  MCU         : STM32F401xx
+ */
+
+#ifndef UART_CONFIGURATION_H_
+#define UART_CONFIGURATION_H_
+
+#define __BAUDRATE__ 115200
+#define __PCLK__	 8000000
+
+#define THRESHOLD_VALUE 500000
+
+#endif /* UART_CONFIGURATION_H_ */
